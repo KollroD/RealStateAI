fef0b72cf2691b484dae249d585ede81f1629b6f
KollroD <107813457+KollroD@users.noreply.github.com>
2026-07-12 00:10:57 +0300

    created async session maker

---
+++ src/shared/database/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+
+DATABASE_URL = "postgresql+asyncpg://admin:password123@localhost:5432/real_estate"
+
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session():
+    async with async_session_maker() as session:
+        yield session
